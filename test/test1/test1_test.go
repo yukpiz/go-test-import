b4b00c386f4c5499fc56c6b71211ec0ec1e01351
yukpiz <yukpiz@gmail.com>
2019-11-27 18:45:49 +0900

    up

---
+++ test/test1/test1_test.go
@@ -0,0 +1,12 @@
+package test1
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/yukpiz/go-test-import/test"
+)
+
+func Test_Test(t *testing.T) {
+	fmt.Println(test.TestValue)
+}
---
+++ test/test_test.go
@@ -0,0 +1,3 @@
+package test
+
+var TestValue = 1
