dbbf931a71513079d554512aea8a08139d454d9b
yukpiz <yukpiz@gmail.com>
2019-11-27 18:38:13 +0900

    up

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yukpiz/go-test-import
+
+go 1.13
---
+++ test1/test1_test.go
@@ -0,0 +1,12 @@
+package test1_test
+
+import (
+	"fmt"
+	"testing"
+
+	. "../test2"
+)
+
+func Test_Test(t *testing.T) {
+	fmt.Println(Test2Value)
+}
